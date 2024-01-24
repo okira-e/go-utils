1c2c6df6fd5d5f1f5e44f1b71b2a12498869ad72
Omar Rafat <omarrafat60@gmail.com>
2024-01-24 09:03:41 +0300

    Added a validation function for missing fields on objects

---
+++ validators.go
@@ -0,0 +1,59 @@
+package go_utils
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// validate returns any missing fields specified in an object.
+func validate[T any](obj T, fields ...string) ([]string, error) {
+	reflectedObject := reflect.ValueOf(&obj).Elem()
+	missingFields := make([]string, 0)
+
+	for _, field := range fields {
+		value := reflectedObject.FieldByName(field)
+		isZero := false
+
+		switch value.Kind() {
+		case reflect.Bool:
+			isZero = !value.Bool()
+		case reflect.Int:
+			fallthrough
+		case reflect.Int8:
+			fallthrough
+		case reflect.Int16:
+			fallthrough
+		case reflect.Int32:
+			fallthrough
+		case reflect.Int64:
+			isZero = value.Int() == 0
+		case reflect.Uint:
+			fallthrough
+		case reflect.Uint8:
+			fallthrough
+		case reflect.Uint16:
+			fallthrough
+		case reflect.Uint32:
+			fallthrough
+		case reflect.Uint64:
+			fallthrough
+		case reflect.Uintptr:
+			isZero = value.Uint() == 0
+		case reflect.Float32:
+			fallthrough
+		case reflect.Float64:
+			isZero = value.Float() == 0
+		case reflect.String:
+			isZero = value.String() == ""
+		default:
+			return []string{}, fmt.Errorf("field %s is not supported", field)
+		}
+
+		if isZero {
+			missingFields = append(missingFields, field)
+		}
+	}
+
+	return missingFields, nil
+}
+
