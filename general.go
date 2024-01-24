e6c28607fa42ee987d904aebc8902bbc85f48e9a
Omar Rafat <omarrafat60@gmail.com>
2024-01-24 10:44:39 +0300

    Added general Go functions file with ternary

---
+++ general.go
@@ -0,0 +1,11 @@
+package go_utils
+
+// Ternary is a substitute for the ternary operator in other languages.
+func Ternary[T any](predicate bool, onTrue T, onFalse T) T {
+	if predicate {
+		return onTrue
+	} else {
+		return onFalse
+	}
+}
+
